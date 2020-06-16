508722bd940e681bc79da0e8ad7b542833284b44
norbert_balogh <norbert.balogh@ewmgroup.com>
2020-06-16 22:18:54 +0200

    modify data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO reservation(booked_date,inactive,on_waiting_list,user_id)
 VALUES
-('2020-06-15',false,false,'user1'),
-('2020-06-15',false,false,'user2'),
-('2020-06-15',false,false,'user3'),
-('2020-06-15',false,false,'user4'),
-('2020-06-15',false,false,'user5');
+('2020-06-16',false,false,'user1'),
+('2020-06-16',false,false,'user2'),
+('2020-06-16',false,false,'user3'),
+('2020-06-16',false,false,'user4'),
+('2020-06-16',false,false,'user5');
